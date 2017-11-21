a852e6771d6832e785a873c3921705c31331040e
shilpapraveen <shilpa@tpgsi.com>
2017-11-21 20:03:19 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,9 @@
+
+import java.awt.Desktop;
+import java.net.URI;
+
+String s = "http://www.google.com";
+Desktop desktop = Desktop.getDesktop();
+desktop.browse(URI.create(s));
+
 System.out.print 'Hello world'
